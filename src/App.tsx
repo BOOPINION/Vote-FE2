86996f103d2c93f0921212d2cbfcb53129f2d279
rony <ahr020532@gmail.com>
2024-04-15 12:26:26 +0900

    f

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ function App() {
     <h1 className="text-3xl font-bold underline">
       ts + tailwindcss 적용함
       음음음asdasd
-      아 내포크레포에서 풀리퀘가 아닌것 같애ㅠㅠ
-
+      아 내포크레포에서 풀리퀘s가 아닌것 같애ㅠㅠ
+sd
     </h1>
   )
 }
