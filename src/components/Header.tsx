2c5727cd2d687b739fca5f84fe1de70275129e0b
honi31 <winner020531@naver.com>
2024-04-15 23:26:47 +0900

    ✨ 헤더 요소 배치 작업
    헤더 요소 배치 작업

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,17 @@
 import React from "react";
-export default function Header() {
-  return <div>헤로나 보여? 제발</div>;
-}
+
+const Header: React.FC = () => {
+  return (
+    <div className="flex w-auto justify-between items-center m-4">
+      <div className="text-xl font-bold">Vote</div>
+      <button className="w-1/4 bg-gray-500 rounded-full">
+        <div className="flex justify-center items-center h-auto flex rounded-full text-lg">
+          <div className="m-2 text-white">홈</div>
+          <div className="m-2 text-white text-lg">투표</div>
+        </div>
+      </button>
+      <button>Profile</button>
+    </div>
+  );
+};
+export default Header;
