94e240925d1440f76beb5797f357391bd54ef1a9
honi31 <winner020531@naver.com>
2024-04-21 16:17:39 +0900

    💄 타이틀 변경 및 부img 추가
    타이틀 변경 및 부img 추가

--- src/components/home/Main.tsx
+++ src/components/home/Main.tsx
@@ -5,13 +5,14 @@ const Main: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-between h-screen">
       <div className="flex flex-col mt-20 mx-12 items-center">
-        <div className="text-4xl font-bold mb-4">당신의 선택은?</div>
-        <div className="w-auto border border-4 border-pink-400 p-2 rounded-lg text-2xl font-bold mb-2">
-          매일 고구마 먹기 vs. 매일 감자 먹기
+        <div className="text-4xl font-bold mb-4">너는 뭐가 좋아?</div>
+        <div className="w-auto border border-4 border-blue-700 p-2 rounded-lg text-2xl font-bold mb-2">
+          발표 빼고 다 하기 vs. 발표만 하기
         </div>
         <div className="text-md text-gray-500 font-semibold">
           로그인하고 더 많은 설문을 선택하세요
         </div>
+        <img src="/Boo.jpeg" />
       </div>
       <Link to="/signup">
         <button className="bg-gray-800 text-white px-6 py-2 rounded-lg font-semibold hover:bg-black mb-24">
