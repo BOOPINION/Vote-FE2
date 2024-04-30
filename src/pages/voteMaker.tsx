b8b165ac74e1c9dca691196b862a131ee12d5dca
hyeryeon <ahr020532@gmail.com>
2024-04-30 23:42:24 +0900

    💄 CreateVote 세부CSS수정

--- src/pages/voteMaker.tsx
+++ src/pages/voteMaker.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
+import { IoIosArrowBack } from 'react-icons/io';
+import { Link } from 'react-router-dom';
 
-//TODO : 컴포넌트 분리
+//TODO: 입력필드 위로 늘어나게 css수정 
 
 type voteItem = {
   id: number;
@@ -35,9 +37,11 @@ const App: React.FC = () => {
 
   //⚡투표 항목 제거 함수
   const handleRemovevote = (id: number) => {
-    setvoteItems(voteItems.filter(item => item.id !== id));
+    if (voteItems.length > 2) {
+      setvoteItems(voteItems.filter(item => item.id !== id));
+    }
   };
-
+  
   //⚡ 주제 선택할때 
   const handleTopicChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setSelectedTopic(event.target.value);
@@ -54,17 +58,17 @@ const App: React.FC = () => {
   });
 
   return (
-    <div className="flex flex-col items-center justify-center p-4 pl-8 pr-8 bg-white rounded-lg shadow">
-      <div className="p-2 flex justify-between items-center w-full">
-        <button className="text-2xl">x</button>
-        투표 만들기
-        <button>완료</button>
+    <div className="flex flex-col items-center justify-center p-4 pl-8 pr-8 bg-white rounded-lg h-screen">
+      <div className="p-2 mb-4 flex justify-between items-center w-full">
+      <Link to="/voteHome" className="text-black">
+          <IoIosArrowBack size="26px" />
+        </Link>
       </div>
       <div className="mt-4 w-full">
         <select
           value={selectedTopic}
           onChange={handleTopicChange}
-          className="w-full rounded-lg border-2 p-2 mb-4"
+          className="w-full  border-b-2 p-2 mb-4"
         >
           <option value="">주제를 선택해주세요</option>
           {topics.map(topic => (
@@ -77,22 +81,22 @@ const App: React.FC = () => {
       <input
         type="text"
         placeholder="제목 내용을 입력해 주세요"
-        className="mt-4 w-full rounded-lg border-2 p-2 mb-2"
+        className="mt-2 w-full rounded-lg text-lg  p-2 mb-2"
       />
-      <input
-        type="text"
+      <textarea
         placeholder="투표 내용을 입력해 주세요"
-        className="mt-4 w-full rounded-lg border-2 p-2 mb-4 "
+        className=" w-full rounded-lg  p-2 mb-4 h-44 resize-y"
       />
-
-      <div className="flex items-center justify-center  w-full overflow-y-hidden">
+  
+      <div className="flex  w-full overflow-y-hidden">
         <div className="w-full max-w-md bg-white shadow-md">
-          <div className="p-4 ">
+          <div className="votebox p-4 mt-4 ">
             <h2 className="text-lg font-semibold text-center">투표</h2>
+            <h4 className='text-sm font-thin text-gray-500 text-center'>항목은 2개이상 5이하로만 가능합니다</h4>
             {voteItems.map((item, index) => (
               <div
                 key={item.id}
-                className="flex items-center border border-gray-300 rounded-lg p-3 my-4"
+                className="flex items-center border border-gray-300 rounded-lg p-1 my-2"
               >
                 <input
                   type="text"
@@ -112,19 +116,23 @@ const App: React.FC = () => {
             ))}
             <button
               onClick={handleAddvote}
-              className="w-full bg-gray-300 text-black py-2 px-4 rounded-lg hover:bg-gray-400"
+              className="w-full bg-gray-300 text-black py-2 px-4 rounded-lg hover:bg-gray-400 mt-2"
               disabled={voteItems.length >= 5}
             >
               + 항목 추가
             </button>
             <div className="text-gray-500 pt-4 text-center">
-              종료일 : {formattedEndDate}
+              자동 종료일 : {formattedEndDate}
             </div>
           </div>
         </div>
       </div>
+      <button className="w-full mt-auto mb-4 bg-gray-800 text-white py-2 px-4 rounded-lg hover:bg-gray-400">
+        투표 만들기
+      </button>
     </div>
   );
+  
 };
 
 export default App;
