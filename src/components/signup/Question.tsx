60825c217e9a20143f0c958338f672a8b65f0e19
honi31 <winner020531@naver.com>
2024-05-06 21:18:39 +0900

    ✨ 회원가입 질문 추가
    이메일, 비밀번호 입력하는 란 추가

--- src/components/signup/Question.tsx
+++ src/components/signup/Question.tsx
@@ -6,7 +6,19 @@ const Question: React.FC<{
   setQuestionNumber: React.Dispatch<React.SetStateAction<number>>;
 }> = ({ questionNumber, setQuestionNumber }) => {
   const [selectedOption, setSelectedOption] = useState<string | null>(null);
+  const [email, setEmail] = useState<string>(""); // 추가: 이메일 입력 상태
+  const [password, setPassword] = useState<string>(""); // 추가: 비밀번호 입력 상태
+  const [confirmPassword, setConfirmPassword] = useState<string>(""); // 추가: 확인용 비밀번호 입력 상태
+
   const questions = [
+    {
+      question: "이메일을 알려주세요",
+      options: ["다음"],
+    },
+    {
+      question: "비밀번호를 설정해주세요",
+      options: ["입력"],
+    },
     {
       question: "당신의 성별이 궁금해요",
       options: ["남성", "여성", "기타"],
@@ -23,32 +35,101 @@ const Question: React.FC<{
 
   const handleOptionSelect = (option: string) => {
     setSelectedOption(option);
-    // 현재 질문 번호가 마지막 질문이 아닐 경우에만 다음 질문으로 이동
     if (questionNumber < questions.length) {
       setQuestionNumber((prevQuestionNumber) => prevQuestionNumber + 1);
     }
   };
 
+  const handleNextButtonClick = () => {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (questionNumber === 1 && !emailPattern.test(email)) {
+      alert("유효한 이메일을 입력해주세요.");
+      return;
+    }
+
+    if (questionNumber === 2 && password !== confirmPassword) {
+      alert("비밀번호가 일치하지 않습니다. 다시 입력해주세요.");
+      return;
+    }
+
+    if (questionNumber < questions.length) {
+      setQuestionNumber((prevQuestionNumber) => prevQuestionNumber + 1);
+    }
+  };
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmPasswordChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setConfirmPassword(e.target.value);
+  };
+
   return (
     <div className="flex flex-col items-start w-auto mx-auto my-8 p-8">
-      {/* 진행률 표시 */}
       <ProgressBar current={questionNumber} total={questions.length} />
       <span className="mt-8 text-lg text-blue-500 font-semibold">
         질문 {questionNumber}
       </span>
-      <h2 className="text-2xl font-bold mt-2 mb-96">
+      <h2 className="text-2xl font-bold mt-2 mb-80">
         {questions[questionNumber - 1].question}
       </h2>
-      {/* 각 질문에 대한 보기를 보여주는 버튼을 생성 */}
-      {questions[questionNumber - 1].options.map((option, index) => (
-        <button
-          key={index}
-          className="mt-8 w-full items-center bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded-full mr-4"
-          onClick={() => handleOptionSelect(option)}
-        >
-          {option}
-        </button>
-      ))}
+      {questionNumber === 1 ? (
+        <>
+          <input
+            type="email"
+            className="w-full px-3 py-2 border rounded-md"
+            placeholder="example@example.com"
+            value={email}
+            onChange={handleEmailChange}
+          />
+          <button
+            className="mt-4 w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+            onClick={handleNextButtonClick}
+          >
+            다음
+          </button>
+        </>
+      ) : questionNumber === 2 ? (
+        <>
+          <input
+            type="password"
+            className="w-full px-3 py-2 border rounded-md mb-4"
+            placeholder="비밀번호"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          <input
+            type="password"
+            className="w-full px-3 py-2 border rounded-md mb-4"
+            placeholder="비밀번호 확인"
+            value={confirmPassword}
+            onChange={handleConfirmPasswordChange}
+          />
+          <button
+            className="mt-4 w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+            onClick={handleNextButtonClick}
+          >
+            다음
+          </button>
+        </>
+      ) : (
+        questions[questionNumber - 1].options.map((option, index) => (
+          <button
+            key={index}
+            className="mt-8 w-full items-center bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded-full mr-4"
+            onClick={() => handleOptionSelect(option)}
+          >
+            {option}
+          </button>
+        ))
+      )}
     </div>
   );
 };
