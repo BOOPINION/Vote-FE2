783965d884742c8386fb02bd5cc2bf7221996c84
honi31 <winner020531@naver.com>
2024-07-10 23:15:38 +0900

    💄 헤더 페이지 switch 버튼 스타일 수정
    헤더 페이지 switch 버튼 스타일 수정

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -1,20 +1,62 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { CgProfile } from "react-icons/cg";
+import { Tabs, TabsList, TabsTrigger } from "../ui/tabs";
 
 const Header: React.FC = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const getValueFromPath = (path: string) => {
+    switch (path) {
+      case "/":
+        return "home";
+      case "/voteHome":
+        return "vote";
+      default:
+        return "home";
+    }
+  };
+
+  const handleTabChange = (value: string) => {
+    switch (value) {
+      case "home":
+        navigate("/");
+        break;
+      case "vote":
+        navigate("/voteHome");
+        break;
+      default:
+        navigate("/");
+        break;
+    }
+  };
   return (
     <div className="flex mx-2 items-center m-4">
       <div className="flex flex-1 flex-grow justify-start text-2xl font-semibold">
         Boopinion
       </div>
-      <div className="flex flex-1 items-center justify-center flex-grow">
-        <Link to="/" className="bg-gray-500 rounded-full px-2 py-1">
-          <div className="text-white">홈</div>
-        </Link>
-        <Link to="/voteHome" className="bg-gray-500 rounded-full px-2 py-1">
-          <div className="text-white">투표</div>
-        </Link>
+      <div className="flex flex-1 items-center justify-center flex-grow transition-all">
+        <Tabs
+          defaultValue={getValueFromPath(location.pathname)}
+          onValueChange={handleTabChange}
+          className="w-32 transition-all"
+        >
+          <TabsList className="w-full h-12 bg-gray-300 transition-all">
+            <TabsTrigger
+              className="w-full font-semibold text-md text-black"
+              value="home"
+            >
+              홈
+            </TabsTrigger>
+            <TabsTrigger
+              className="w-full font-semibold text-md text-black"
+              value="vote"
+            >
+              투표
+            </TabsTrigger>
+          </TabsList>
+        </Tabs>
       </div>
       <div className="flex flex-1 flex-grow justify-end">
         <Link to="login">
